c00b6f7bf75758690d4936c126ec81662dce0b1c
Dagu9 <davide.guidolin@studenti.unitn.it>
2025-07-27 11:08:47 +0200

    Masked attention

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 @dataclass
 class T5Config:
-    block_size: int = 256 # context length
+    block_size: int = 1024 # max context length
     n_embd: int = 256 # embedding size
     mlp_hidden_size: int = 4 * n_embd # mlp hidden size
     n_head: int = 4 # number of attention heads
