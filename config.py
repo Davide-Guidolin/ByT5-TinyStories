b197545044f13dc95b9858b6cb0f239706da7144
Dagu9 <davide.guidolin@studenti.unitn.it>
2025-07-31 18:42:32 +0200

    Relative position bias

--- config.py
+++ config.py
@@ -5,10 +5,13 @@ class T5Config:
     vocab_size: int = 100 # vocabulary size
     block_size: int = 1024 # max context length
     n_embd: int = 256 # embedding size
-    mlp_hidden_size: int = 4 * n_embd # mlp hidden size
     n_head: int = 4 # number of attention heads
     n_layer_enc: int = 4 # number of encoder layers
     n_layer_dec: int = 4 # number of decoder layers
+    
+    n_position_buckets: int = 32 # number of embeddings for relative positional encoding
+    max_bucket_offset: int = 128 # max offset used to assign a bucket
+    mlp_hidden_size: int = 4 * n_embd # mlp hidden size
     
     attn_dropout: float = 0.1 # attention dropout
     attn_proj_dropout: float = 0.1 # attention dropout
--- t5.py
+++ t5.py
@@ -38,7 +38,7 @@ class MultiHeadSelfAttention(nn.Module):
             mask = torch.tril(torch.ones(self.block_size, self.block_size)).view(1, 1, self.block_size, self.block_size)
             self.register_buffer("bias", mask)
         
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, position_bias: torch.Tensor = None) -> torch.Tensor:
         
         # batch size, sequence length, embedding dimensionality (n_embd)
         B, T, C = x.size()
@@ -53,7 +53,13 @@ class MultiHeadSelfAttention(nn.Module):
         v = v.view(B, T, self.n_head, self.dim_head).transpose(1, 2) # (B, n_head, T, dim_head)
         
         # attention calculation
-        attn = (q @ k.transpose(-2, -1)) / self.scale # (B, n_head, T, T)
+        attn = (q @ k.transpose(-2, -1)) # (B, n_head, T, T)
+        
+        if position_bias is not None:
+            attn = attn + position_bias
+            
+        attn = attn / self.scale
+        
         if self.is_causal:
             # mask future tokens
             mask = self.bias[:, :, :T, :T] == 0
@@ -97,7 +103,7 @@ class CrossAttention(nn.Module):
         self.attn_dropout = nn.Dropout(p=attn_droput)
         self.proj_dropout = nn.Dropout(p=attn_proj_droput)
 
-    def forward(self, x_q: torch.Tensor, x_kv: torch.Tensor) -> torch.Tensor:
+    def forward(self, x_q: torch.Tensor, x_kv: torch.Tensor, position_bias: torch.Tensor = None) -> torch.Tensor:
         
         B, T_q, C = x_q.size()
         _, T_kv, _ = x_kv.size()
@@ -110,7 +116,13 @@ class CrossAttention(nn.Module):
         k = k.view(B, T_kv, self.n_head, self.dim_head).transpose(1, 2)
         v = v.view(B, T_kv, self.n_head, self.dim_head).transpose(1, 2)
         
-        attn = (q @ k.transpose(-2, -1)) / self.scale        
+        attn = (q @ k.transpose(-2, -1))
+        
+        if position_bias is not None:
+            attn = attn + position_bias
+            
+        attn = attn / self.scale
+   
         attn = F.softmax(attn, dim=-1)
         attn = self.attn_dropout(attn)
         y = attn @ v
@@ -168,8 +180,8 @@ class EncoderBlock(nn.Module):
         self.mlp = MLP(config.n_embd, config.mlp_hidden_size, config.mlp_dropout)
         self.dropout = nn.Dropout(config.skip_conn_dropout)
         
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = x + self.dropout(self.attn(self.ln_1(x)))
+    def forward(self, x: torch.Tensor, position_bias: torch.Tensor = None) -> torch.Tensor:
+        x = x + self.dropout(self.attn(self.ln_1(x), position_bias=position_bias))
         x = x + self.dropout(self.mlp(self.ln_2(x)))
         
         return x        
@@ -201,9 +213,9 @@ class DecoderBlock(nn.Module):
         self.mlp = MLP(config.n_embd, config.mlp_hidden_size, config.mlp_dropout)
         self.dropout = nn.Dropout(config.skip_conn_dropout)
         
-    def forward(self, x: torch.Tensor, encoder_out: torch.Tensor) -> torch.Tensor:
-        x = x + self.dropout(self.causal_attn(self.ln_1(x)))
-        x = x + self.dropout(self.cross_attn(self.ln_2(x), encoder_out))
+    def forward(self, x: torch.Tensor, encoder_out: torch.Tensor, position_bias_self_attn: torch.Tensor = None, position_bias_cross_attn: torch.Tensor = None) -> torch.Tensor:
+        x = x + self.dropout(self.causal_attn(self.ln_1(x), position_bias_self_attn))
+        x = x + self.dropout(self.cross_attn(self.ln_2(x), encoder_out, position_bias_cross_attn))
         x = x + self.dropout(self.mlp(self.ln_3(x)))
         
         return x
@@ -215,11 +227,11 @@ class T5(nn.Module):
         
         self.config = config
         
+        self.relative_wpe = nn.Embedding(config.n_position_buckets, config.n_head)
+        
         self.transformer = nn.ModuleDict(dict(
             # word token embedding
             wte = nn.Embedding(config.vocab_size, config.n_embd),
-            # word positional embedding
-            wpe = nn.Embedding(config.block_size, config.n_embd),
             # dropout for embedding
             drop = nn.Dropout(config.embd_dropout),
             # encoder
@@ -234,6 +246,47 @@ class T5(nn.Module):
         # Weight tying with embedding
         self.lm_head.weight = self.transformer.wte.weight
         
+    def _relative_position_bucket(self, relative_position: torch.Tensor, num_buckets: int = 32, max_distance: int = 128, bidirectional: bool = True) -> torch.Tensor:
+        """
+        Calculates the bucket index for a given relative position.
+
+        Args:
+            relative_position: A tensor of relative positions.
+            num_buckets: The total number of buckets.
+            max_distance: The maximum distance to consider before bucketing.
+            bidirectional: Whether the attention is bidirectional (encoder) or not (decoder).
+   
+        Returns:
+           A tensor of bucket indices.
+        """
+        res = 0
+        
+        # negate relative positions -> think about relative position as 'distance into the past' e.g. 2 means the key is 2 step behind the query
+        relative_position = -relative_position
+        
+        if bidirectional:
+            # if bidirectional half buckets are used for negative values, half for positive values
+            num_buckets = num_buckets // 2
+            res += (relative_position < 0).to(torch.long) * num_buckets # adds num_bucket to negative positions, e.g. for 32 bucket 0 - 15 will be for positive positions, 16 - 31 for negative
+            relative_position = relative_position.abs()
+        else:
+            # if not bidirectional use all the available buckets
+            relative_position = torch.max(relative_position, torch.zeros_like(relative_position)) # if causal attention, clip negative values (i.e. key ahead query -> future tokens)
+            
+        # half of available buckets, i.e. original_num_buckets // 4 for bidirectional and original_num_buckets // 2 for non bidirectional
+        # is used for direct mapping with absolute value, the other half is used for log scaling
+        max_exact = num_buckets // 2
+        is_small = relative_position < max_exact
+        
+        # log scaling for positions >= max_exact
+        val_if_large = max_exact + (torch.log(relative_position.float() / max_exact) / torch.log(torch.tensor(max_distance / max_exact)) * (num_buckets - max_exact)).to(torch.long)
+        # make sure bucket index does not exceed the maximum
+        val_if_large = torch.min(val_if_large, torch.full_like(val_if_large, num_buckets - 1))
+
+        res += torch.where(is_small, relative_position, val_if_large)
+        return res
+        
+        
     def forward(self, src_idx: torch.Tensor, trg_idx: torch.Tensor = None) -> torch.Tensor:
         _, T_src = src_idx.size()
         _, T_trg = trg_idx.size()
@@ -241,26 +294,39 @@ class T5(nn.Module):
         assert T_trg <= self.config.block_size, f"Cannot use sequence of length {T_trg} as target, maximum block size is {self.config.block_size}"
         
         # Encoder
+        enc_pos = torch.arange(0, T_src, dtype=torch.long, device=src_idx.device)
+        enc_relative_position = enc_pos[None, :] - enc_pos[:, None]
+        enc_rp_bucket = self._relative_position_bucket(enc_relative_position, self.config.n_position_buckets, self.config.max_bucket_offset)
+          
+        enc_position_bias = self.relative_wpe(enc_rp_bucket) # (T_src, T_src, n_head)
+        enc_position_bias = enc_position_bias.permute(2, 0, 1).unsqueeze(0) # (1, n_head, T_src, T_src)
+    
         src_emb = self.transformer.wte(src_idx)
-        pos = torch.arange(0, T_src, dtype=torch.long, device=src_idx.device)
-        src_pos_enc = self.transformer.wpe(pos)
-        enc_x = src_emb + src_pos_enc
-        enc_x = self.transformer.drop(enc_x)
+        enc_x = self.transformer.drop(src_emb)
         
         for block in self.transformer.encoder:
-            enc_x = block(enc_x)
+            enc_x = block(enc_x, position_bias=enc_position_bias)
         
         encoder_out = self.transformer.ln_enc(enc_x)
         
         # Decoder
+        dec_pos = torch.arange(0, T_trg, dtype=torch.long, device=src_idx.device)
+        dec_relative_position = dec_pos[None, :] - dec_pos[:, None]
+        dec_rp_bucket = self._relative_position_bucket(dec_relative_position, self.config.n_position_buckets, self.config.max_bucket_offset, bidirectional=False)
+          
+        dec_position_bias_self_attn = self.relative_wpe(dec_rp_bucket) # (T_trg, T_trg, n_head)
+        dec_position_bias_self_attn = dec_position_bias_self_attn.permute(2, 0, 1).unsqueeze(0) # (1, n_head, T_trg, T_trg)
+        
+        dec_relative_position_cross_attn = enc_pos[None, :] - dec_pos[:, None]
+        dec_rp_bucket_cross_attn = self._relative_position_bucket(dec_relative_position_cross_attn, self.config.n_position_buckets, self.config.max_bucket_offset)
+        dec_position_bias_cross_attn = self.relative_wpe(dec_rp_bucket_cross_attn)
+        dec_position_bias_cross_attn = dec_position_bias_cross_attn.permute(2, 0, 1).unsqueeze(0)
+        
         trg_emb = self.transformer.wte(trg_idx)
-        pos = torch.arange(0, T_trg, dtype=torch.long, device=trg_idx.device)
-        trg_pos_enc = self.transformer.wpe(pos)
-        dec_x = trg_emb + trg_pos_enc
-        dec_x = self.transformer.drop(dec_x)
+        dec_x = self.transformer.drop(trg_emb)
         
         for block in self.transformer.decoder:
-            dec_x = block(dec_x, encoder_out)
+            dec_x = block(dec_x, encoder_out, position_bias_self_attn=dec_position_bias_self_attn, position_bias_cross_attn=dec_position_bias_cross_attn)
 
         decoder_out = self.transformer.ln_dec(dec_x)
         
