c27b5bebb50aa85a410bcb3c239b37f886f05318
Dagu9 <davide.guidolin@studenti.unitn.it>
2025-08-02 12:13:14 +0200

    Dataset with span corruption implementation

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ from dataclasses import dataclass
 
 @dataclass
 class T5Config:
-    vocab_size: int = 100 # vocabulary size
+    vocab_size: int = 258 # vocabulary size. 0 - 255 bytes, 256 PAD, 257 EOS
     block_size: int = 1024 # max context length
     n_embd: int = 256 # embedding size
     n_head: int = 4 # number of attention heads
-    n_layer_enc: int = 4 # number of encoder layers
     n_layer_dec: int = 4 # number of decoder layers
+    n_layer_enc: int = 3 * n_layer_dec # number of encoder layers
     
     n_position_buckets: int = 32 # number of embeddings for relative positional encoding
     max_bucket_offset: int = 128 # max offset used to assign a bucket
@@ -18,3 +18,11 @@ class T5Config:
     mlp_dropout: float = 0.1 # mlp dropout
     skip_conn_dropout: float = 0.1 # dropout on the skip connection
     embd_dropout: float = 0.1 # dropout on the embedding
+    
+@dataclass
+class DataConfig:
+    pad_token_id: int = 256
+    eos_token_id: int = 257
+    mask_pct: float = 0.15 # % of token to mask
+    mean_span_corruption_length: int = 20 # mean span corruption length
+    
--- data.py
+++ data.py
@@ -1,24 +1,123 @@
-from datasets import load_dataset, Dataset 
+from datasets import load_dataset, Dataset as HFDataset 
+import torch
+from torch.utils.data import Dataset
+from config import DataConfig
+import numpy as np
+from copy import deepcopy
 
-class Data:
+class TinyStories:
     def __init__(self, dataset_id: str = "roneneldan/TinyStories"):
         self.dataset_id = dataset_id
         self.dataset = load_dataset(self.dataset_id)
         
-    def get_train(self) -> Dataset:
+    def get_train(self) -> HFDataset:
         if 'train' in self.dataset:
             return self.dataset['train']
         
         raise ValueError("Train split not found in the dataset")
     
-    def get_validation(self) -> Dataset:
+    def get_validation(self) -> HFDataset:
         if 'validation' in self.dataset:
             return self.dataset['validation']
         
-        raise ValueError("Validation split not found in the dataset")    
+        raise ValueError("Validation split not found in the dataset")
+    
+class TinyStoriesDataset(Dataset):
+    def __init__(self, config: DataConfig, ts_dataset: TinyStories, split: str ="train"):
+        
+        assert split in ["train", "validation"], f"Error creating the dataset. Split should be either train or validation. Got {split}."
+        
+        self.config = config
+        self.data = None
+        if split == "train":
+            self.data = ts_dataset.get_train()
+        elif split == "validation":
+            self.data = ts_dataset.get_validation()
+            
+            
+    def __len__(self) -> int:
+        return len(self.data)
+    
+    def __getitem__(self, idx: int) -> tuple[torch.Tensor]:
+        story = self.data[idx]['text']
+        
+        # convert to a list of utf-8 bytes
+        bytes_ids = list(story.encode("utf-8"))
+        
+        # span corruption
+        source_ids, target_ids = self.span_corruption(bytes_ids)
+        
+        # add EOS
+        source_ids.append(self.config.eos_token_id)
+        target_ids.append(self.config.eos_token_id)
+        
+        # convert to tensor
+        source = torch.tensor(source_ids, dtype=torch.long)
+        target = torch.tensor(target_ids, dtype=torch.long)
+        
+        return source, target
+    
+    def span_corruption(self, x: list) -> tuple[list]:
+        
+        # track masked bytes
+        is_masked = np.zeros((1, len(x)), dtype=bool)
+        masked_spans = [] # contains span as a tuple (start_idx, end_idx, sentinel_id)
+        
+        # number of token to mask
+        total_to_mask = round(len(x) * self.config.mask_pct)
+        # token already masked
+        masked_count = 0
+        # sentinel id. Starts from 255 and goes down
+        sentinel_id = 255
+        
+        while masked_count < total_to_mask:
+            # Generate span length using poisson distribution
+            span_length = np.random.poisson(lam=self.config.mean_span_corruption_length)
+            span_length = max(span_length, 1)
+            
+            # choose start index
+            start_idx = np.random.randint(0, len(x) - span_length)
+            # prevent overlapping spans
+            while any(is_masked[start_idx : start_idx + span_length]):
+                start_idx = np.random.randint(0, len(x) - span_length)
+            
+            is_masked[start_idx:start_idx + span_length] = True
+                
+            masked_spans.append((
+                start_idx,
+                start_idx + span_length,
+                sentinel_id
+            ))
+            
+            masked_count += span_length
+            sentinel_id -= 1
+        
+        # sort by start index
+        masked_spans.sort(key=lambda span: span[0])
+        
+        source_ids = []
+        target_ids = []
+        
+        current_pos = 0
+        for span in masked_spans:
+            target_ids.append(span[2]) # sentinel
+            target_ids.extend(x[span[0]: span[1]]) # corrupted bytes
+            
+            source_ids.extend(x[current_pos:span[0]]) # non masked bytes
+            source_ids.append(span[2]) # sentinel
+            current_pos = span[1] # move position to the end of span
+            
+        source_ids.extend(x[current_pos:])
+            
+        return source_ids, target_ids
+        
     
 if __name__ == "__main__":
-    data = Data()
-    print(data.get_train()[0])
-    print(data.get_validation()[0])
-
+    config = DataConfig()
+    ds = TinyStoriesDataset(config, ts_dataset=TinyStories(), split="train")
+    
+    x, y = ds[0]
+    
+    print(x)
+    print("------")
+    print(y)
